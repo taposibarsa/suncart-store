4065a2defc6dc28d8e280b5863ed324ecfcc255b
taposi barsa <taposibarsa@gmail.com>
2026-05-04 18:08:22 +0600

    baseurl changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,8 +6,8 @@ const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db("betterAuthUserDB");
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
 
