0c5c5a1a4d8b1df6391e559d382525caa2ace9eb
taposi barsa <taposibarsa@gmail.com>
2026-05-04 18:54:03 +0600

    auth.js file edited

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,23 +3,23 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.AUTH_DB_URI);
+await client.connect();
 const db = client.db("betterAuthUserDB");
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
-  database: mongodbAdapter(db, {
-    client
-  }),
 
-  emailAndPassword: { 
-    enabled: true, 
+  database: mongodbAdapter(db, { client }),
+
+  emailAndPassword: {
+    enabled: true,
     minPasswordLength: 6,
   },
-    socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID , 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET  
-        }, 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+  },
 });
